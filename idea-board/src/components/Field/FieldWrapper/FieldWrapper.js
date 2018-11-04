79f04db49669c1effc73edfe1e25ebf7baada061
Melwyn Furtado <melwynfurtado@gmail.com>
2018-11-04 00:19:35 +0000

    refactor Field component into FieldWrapper

---
+++ idea-board/src/components/Field/FieldWrapper/FieldWrapper.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class FieldWrapper extends Component {
+  render() {
+    const { id, label, children } = this.props;
+    return (
+      <div className={`form-group row ${id}`}>
+        <label htmlFor={id} className="col-sm-2 col-form-label">{label}: </label>
+        <div className="col-sm-7">
+          { children }
+        </div>
+      </div>    
+    )
+  }
+}
+
+export default FieldWrapper;
+
